97c9d1194ad6f16b3677f8ce0dd33ae84e339bcd
Tristan Deitzer <tristandeitzer@gmail.com>
2020-03-10 15:04:07 -0400

    Routes Functioning, and removed faulty 'main' from package.json so nodemon operates correctly

--- server/router.js
+++ server/router.js
@@ -1,3 +1,22 @@
 let router = require("express").Router();
 
+router
+  .route("/cat")
+  .get((req, res) => {
+    res.send("GET cat");
+  })
+  .post((req, res) => {
+    res.send("POST cat");
+  })
+  .put((req, res) => {
+    res.send("PUT cat");
+  })
+  .delete((req, res) => {
+    res.send("DELETE cat");
+  });
+
+router.get("/funds", (req, res) => {
+  res.send("GET funds");
+});
+
 module.exports = router;
