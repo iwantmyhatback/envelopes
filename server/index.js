acf8bdcd7e823c029a2d0cd94b5b2dafd9b6c9d4
Tristan Deitzer <tristandeitzer@gmail.com>
2020-03-10 15:52:47 -0400

    Confirming React Will Serve statically

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ let express = require("express");
 let morgan = require("morgan");
 let bodyParser = require("body-parser");
 let router = require("./router.js");
+let path = require("path");
 let app = express();
 let port = 5555;
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "../client/dist/")));
 
 app.use("/", router);
 
