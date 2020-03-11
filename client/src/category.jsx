19b76ec44c78ba30efe437b6072e58f75953d152
Mayanne Chess <mayplusmay@gmail.com>
2020-03-11 11:27:07 -0400

    fixed mismatched names in category

--- client/src/category.jsx
+++ client/src/category.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 import AddMoneyForm from "./addMoneyForm.jsx";
 
-const Category = ({category, updateCatHandler, updateFundsHandler, deleteHandler}) => {
+const Category = ({category, updateCategoryHandler, updateFundsHandler, deleteHandler}) => {
   return (
   <div className="box">
     <h4>{category.name}</h4>
     <div>${category.now}</div>
     <br />
-    <AddMoneyForm category={category} updateHandler={updateHandler} />
+    <AddMoneyForm category={category} updateCategoryHandler={updateCategoryHandler} />
     <button type="button" onClick={() => {
       const clearAmount = category.now * -1;
       updateFundsHandler(clearAmount);
-      updateCatHandler(category.id, "now", 0);
+      updateCategoryHandler(category.id, "now", 0);
     }}>Clear</button>
     <button type="button" onClick={() => deleteHandler(category.id)}>Delete</button>
   </div>
